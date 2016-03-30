05dc48317514a80aa003e7d1f27d23a591d5beb2
luhtonen <luhtonen@gmail.com>
2016-03-30 08:01:17 +0200

    Splitting reducers

--- app/reducers.js
+++ app/reducers.js
@@ -5,6 +5,31 @@ const initialState = {
   todos: []
 };
 
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [
+        ...state,
+        {
+          text: action.text,
+          completed: false
+        }
+      ];
+    case COMPLETE_TODO:
+      return state.map((todo, index) => {
+        if (index === action.index) {
+          return {
+            ...todo,
+            completed: true
+          }
+        }
+        return todo;
+      });
+    default:
+      return state;
+  }
+};
+
 export const todoApp = (state = initialState, action) => {
   switch (action.type) {
     case SET_VISIBILITY_FILTER:
@@ -13,28 +38,10 @@ export const todoApp = (state = initialState, action) => {
         visibilityFilter: action.filter
       };
     case ADD_TODO:
-      return {
-        ...state,
-        todos: [
-          ...state.todos,
-          {
-            text: action.text,
-            completed: false
-          }
-        ]
-      };
     case COMPLETE_TODO:
       return {
         ...state,
-        todos: state.todos.map((todo, index) => {
-          if (index === action.index) {
-            return {
-              ...todo,
-              completed: true
-            }
-          }
-          return todo;
-        })
+        todos: todos(state.todos, action)
       };
     default:
       return state;
