4a5c54bc903daeb5c0758b11f11115d2fb3acb70
luhtonen <luhtonen@gmail.com>
2016-04-02 19:02:40 +0200

    Fixing import

--- async/app/index.js
+++ async/app/index.js
@@ -1,7 +1,7 @@
 import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
-import { selectSubreddit, fetchPosts } from './actions';
+import { selectSubreddit, fetchPostsIfNeeded } from './actions';
 import rootReducer from './reducers';
 
 const loggerMiddleware = createLogger();
